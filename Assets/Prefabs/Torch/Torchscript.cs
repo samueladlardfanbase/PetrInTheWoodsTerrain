4a0aa0c5a615329d633857cfde963562675a456d
Samuel Adlard <sammyadlard@gmail.com>
2021-08-18 13:20:20 +1000

    more stuff

--- Assets/Prefabs/Torch/Torchscript.cs
+++ Assets/Prefabs/Torch/Torchscript.cs
@@ -4,19 +4,38 @@ using UnityEngine;
 
 public class Torchscript : MonoBehaviour
 {
-    public GameObject Torch;
+    public GameObject Torchbit1;
+    public GameObject Torchbit2;
+    public GameObject Torchbit3;
+    public Animator anim;
     bool equiped = false;
+
+    private void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+
     void Update()
     {
         if (Input.GetKeyDown("1") && equiped == false)
         {
-            Torch.SetActive(true);
+            Torchbit1.GetComponent<MeshRenderer>().enabled = true;
+            Torchbit2.GetComponent<MeshRenderer>().enabled = true;
+            Torchbit3.GetComponent<MeshRenderer>().enabled = true;
             equiped = true;
         }
         else if(Input.GetKeyDown("1") && equiped == true)
         {
             equiped = false;
-            Torch.SetActive(false);
+            Torchbit1.GetComponent<MeshRenderer>().enabled = false;
+            Torchbit2.GetComponent<MeshRenderer>().enabled = false;
+            Torchbit3.GetComponent<MeshRenderer>().enabled = false;
+        }
+
+        if (equiped == true && Input.GetMouseButtonDown(0))
+        {
+            anim.Play("Cylinder_001|Cylinder_001Action");
         }
     }
 }
