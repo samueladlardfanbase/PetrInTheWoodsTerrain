82a781d3c6e0b3b4a7460d9ec937224daabd8693
Samuel Adlard <sammyadlard@gmail.com>
2021-08-26 17:39:40 +1000

    Added stuff

--- Assets/Prefabs/Torch/Torchscript.cs
+++ Assets/Prefabs/Torch/Torchscript.cs
@@ -7,9 +7,11 @@ public class Torchscript : MonoBehaviour
     public GameObject Torchbit1;
     public GameObject Torchbit2;
     public GameObject Torchbit3;
+    public GameObject torchlight;
     public Animator anim;
+    
     bool equiped = false;
-
+    bool torchon = false;
     private void Start()
     {
         anim = gameObject.GetComponent<Animator>();
@@ -23,6 +25,7 @@ public class Torchscript : MonoBehaviour
             Torchbit1.GetComponent<MeshRenderer>().enabled = true;
             Torchbit2.GetComponent<MeshRenderer>().enabled = true;
             Torchbit3.GetComponent<MeshRenderer>().enabled = true;
+            
             equiped = true;
         }
         else if(Input.GetKeyDown("1") && equiped == true)
@@ -31,11 +34,27 @@ public class Torchscript : MonoBehaviour
             Torchbit1.GetComponent<MeshRenderer>().enabled = false;
             Torchbit2.GetComponent<MeshRenderer>().enabled = false;
             Torchbit3.GetComponent<MeshRenderer>().enabled = false;
+            torchlight.SetActive(false);
+            torchon = false;
         }
 
-        if (equiped == true && Input.GetMouseButtonDown(0))
+        if (equiped == true && Input.GetMouseButtonDown(0) && torchon == false)
         {
+
             anim.Play("Cylinder_001|Cylinder_001Action");
+            torchlight.SetActive(true);
+            torchon = true;
+        }
+        else if(equiped == true && Input.GetMouseButtonDown(0) && torchon == true)
+        {
+            
+            anim.Play("Cylinder_001|Cylinder_001Action");
+            torchlight.SetActive(false);
+            torchon = false;
         }
     }
+
+
+    
+
 }
